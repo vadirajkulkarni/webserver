1757083c863803d098ad707287fd2b3166e22fd7
Vadiraj Kulkarni <vadirajkulkarni@mac.local>
2024-03-07 18:33:42 +1100

    Added server.go

---
+++ pkg/server.go
@@ -0,0 +1,19 @@
+package pkg
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
